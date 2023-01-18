631a2d5cd9aa56dd5209b04a2aa7f8d1ccc95fd2
Anggi Fergian <anggi.fergian@koinworks.com>
2023-01-19 01:22:13 +0700

    Add tutorial 2

---
+++ lib/configs/environment.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class Environment {
+  static String get fileName => kReleaseMode ? ".env.production" : ".env";
+  static String get apiUrl => dotenv.env['API_URL']!;
+  static String get jsonPlaceHolderUrl => dotenv.env['JPH_URL']!;
+}

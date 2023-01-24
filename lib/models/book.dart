c408ca37a3c050f18c898121c61cfed9790a3dc9
Anggi Fergian <anggi.fergian@koinworks.com>
2023-01-24 21:52:08 +0700

    Finish book app

--- lib/models/book.dart
+++ lib/models/book.dart
@@ -14,3 +14,74 @@ class Book {
         json['image'], json['url']);
   }
 }
+
+class BookListResponse {
+  String? error;
+  String? total;
+  List<Book>? books;
+
+  BookListResponse(this.error, this.total, this.books);
+
+  BookListResponse.fromJson(Map json) {
+    error = json['error'];
+    total = json['total'];
+
+    if (json['books'] != null) {
+      books = <Book>[];
+      json['books'].forEach((item) {
+        books!.add(Book.fromJson(item));
+      });
+    }
+  }
+}
+
+class BookDetail {
+  final String error;
+  final String title;
+  final String subtitle;
+  final String authors;
+  final String publisher;
+  final String isbn10;
+  final String isbn13;
+  final String pages;
+  final String year;
+  final String rating;
+  final String desc;
+  final String price;
+  final String image;
+  final String url;
+
+  BookDetail(
+      this.error,
+      this.title,
+      this.subtitle,
+      this.authors,
+      this.publisher,
+      this.isbn10,
+      this.isbn13,
+      this.pages,
+      this.year,
+      this.rating,
+      this.desc,
+      this.price,
+      this.image,
+      this.url);
+
+  factory BookDetail.fromJson(Map json) {
+    return BookDetail(
+        json['error'],
+        json['title'],
+        json['subtitle'],
+        json['authors'],
+        json['publisher'],
+        json['isbn10'],
+        json['isbn13'],
+        json['pages'],
+        json['year'],
+        json['rating'],
+        json['desc'],
+        json['price'],
+        json['image'],
+        json['url']);
+  }
+}
---
+++ lib/views/book_app/related_book.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
+import 'package:flutter_edspert/models/book.dart';
+
+class RelatedBook extends StatelessWidget {
+  const RelatedBook({
+    Key? key,
+    required this.book,
+  }) : super(key: key);
+
+  final Book? book;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 120,
+      child: Card(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CachedNetworkImage(
+              imageUrl: book!.image,
+              height: 120,
+              fit: BoxFit.cover,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    book!.title,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    textAlign: TextAlign.left,
+                    style: const TextStyle(fontSize: 12),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    book!.price,
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 14),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

d2a7f42864466a4dd1d2a7ac35401816e181ad7f
Anggi Fergian <anggi.fergian@koinworks.com>
2023-01-24 21:53:13 +0700

    Add splash screen

--- lib/components/splash_screen.dart
+++ lib/components/splash_screen.dart
@@ -1,6 +1,7 @@
-import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_edspert/views/jph_list.dart';
+
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter_edspert/views/book_app/home_book.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -17,17 +18,20 @@ class SplashScreenState extends State<SplashScreen> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: const [
           Text(
-            'Grapdeia',
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+            'Grapedia',
+            style: TextStyle(
+                fontSize: 24, fontWeight: FontWeight.w600, color: Colors.white),
           )
         ],
       ),
     );
 
     return AnimatedSplashScreen(
-        splash: splash,
-        splashTransition: SplashTransition.fadeTransition,
-        duration: 700,
-        nextScreen: const JPHList());
+      splash: splash,
+      splashTransition: SplashTransition.fadeTransition,
+      duration: 700,
+      nextScreen: const BookHome(),
+      backgroundColor: Colors.blue,
+    );
   }
 }

ad120374dfc064f2b78732392929cf139e645504
Anggi Fergian <anggi.fergian@koinworks.com>
2023-02-04 22:15:46 +0700

    Update material theme

--- lib/configs/theme.dart
+++ lib/configs/theme.dart
@@ -4,7 +4,7 @@ import 'package:flutter_edspert/configs/constant.dart';
 
 ThemeData lightTheme(BuildContext context) {
   return ThemeData(
-      scaffoldBackgroundColor: Colors.white,
+      scaffoldBackgroundColor: const Color(0xFFF3F7F8),
       textTheme: const TextTheme(
         labelLarge: TextStyle(fontSize: 37),
         labelMedium: TextStyle(fontSize: 22),
@@ -13,7 +13,7 @@ ThemeData lightTheme(BuildContext context) {
       ),
       appBarTheme: const AppBarTheme(
           centerTitle: false,
-          backgroundColor: Colors.transparent,
+          backgroundColor: Color(0xFF3A7FD5),
           elevation: 0,
           systemOverlayStyle: SystemUiOverlayStyle.dark,
           toolbarTextStyle: TextStyle(color: textColorLightTheme)),
@@ -28,6 +28,5 @@ ThemeData lightTheme(BuildContext context) {
               minimumSize: const Size(double.infinity, 48),
               shape: const StadiumBorder(),
               backgroundColor: primaryColor,
-              textStyle: const TextStyle(
-                  color: Colors.white, fontWeight: FontWeight.w600))));
+              textStyle: const TextStyle(color: Colors.white, fontWeight: FontWeight.w600))));
 }

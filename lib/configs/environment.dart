4b2c273fba4718042282ec885e200792c5f81988
Anggi Fergian <anggi.fergian@koinworks.com>
2023-02-04 22:15:34 +0700

    Update environment

--- lib/configs/environment.dart
+++ lib/configs/environment.dart
@@ -3,7 +3,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Environment {
   static String get fileName => kReleaseMode ? ".env.production" : ".env";
-  static String get apiUrl => dotenv.env['API_URL']!;
   static String get jsonPlaceHolderUrl => dotenv.env['JPH_URL']!;
   static String get itBookApi => dotenv.env['ITBOOK_API']!;
   static String get edspertApi => dotenv.env['EDSPERT_API']!;

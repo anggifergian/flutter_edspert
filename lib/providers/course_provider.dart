ff915879bc26afc121cbabde8d8d9f2fae4b16d9
Anggi Fergian <anggi.fergian@koinworks.com>
2023-02-04 22:17:05 +0700

    Add course provider & service

--- lib/providers/course_provider.dart
+++ lib/providers/course_provider.dart
@@ -1,22 +1,28 @@
 import 'package:flutter/foundation.dart';
-import 'package:flutter_edspert/models/final_project_app/course.dart';
 
+import 'package:flutter_edspert/models/final_project_app/course.dart';
 import 'package:flutter_edspert/services/final_project/course_service.dart';
 
 class CourseProvider with ChangeNotifier {
   bool isLoading = false;
 
-  List<Course>? data;
+  List<Course>? course = [];
 
-  void getAll({int item = 1}) async {
+  Future<void> getAll({int item = 1}) async {
     isLoading = true;
     notifyListeners();
 
-    var response = await CourseService().getAll();
-    print(response);
+    try {
+      var response = await CourseService().getAll();
 
-    await Future.delayed(const Duration(seconds: 3));
-    isLoading = false;
-    notifyListeners();
+      if (response != null) {
+        isLoading = false;
+        course = response;
+        notifyListeners();
+      }
+    } catch (e) {
+      isLoading = false;
+      notifyListeners();
+    }
   }
 }
--- lib/services/final_project/course_service.dart
+++ lib/services/final_project/course_service.dart
@@ -1,32 +1,19 @@
-import 'dart:convert';
-import 'dart:io';
-
-import 'package:http/http.dart' as http;
-
-import 'package:flutter_edspert/configs/environment.dart';
+import 'package:flutter_edspert/configs/dio_client.dart';
 import 'package:flutter_edspert/models/final_project_app/course.dart';
 
 class CourseService {
   final String _userEmail = 'testerngbayu@gmail.com';
-  final String _xApiKey = '18be70c0-4e4d-44ff-a475-50c51ece99a0';
 
-  Future<List<Course>> getAll({String major = 'IPA'}) async {
-    try {
-      print('start...');
-      final uri = Uri.parse('${Environment.edspertApi}/exercise/data_course?major_name=$major&user_email=$_userEmail');
-      final response = await http.get(uri, headers: {'x-api-key': _xApiKey});
+  final DioClient dioClient = DioClient();
 
-      print('finish...');
-      switch (response.statusCode) {
-        case 200:
-          final data = jsonDecode(response.body);
-          return CourseListResponse.fromJson(data).data!;
-        default:
-          print('error...${response.reasonPhrase}');
-          throw Exception(response.reasonPhrase);
-      }
-    } on SocketException catch (_) {
-      rethrow;
+  Future<List<Course>?> getAll({String major = 'IPA'}) async {
+    var path = '/exercise/data_course?major_name=$major&user_email=$_userEmail';
+    var response = await dioClient.dio.get(path);
+
+    if (response.statusCode == 200) {
+      return CourseListResponse.fromJson(response.data).data;
+    } else {
+      throw 'Unable to retrieve course data.';
     }
   }
 }
